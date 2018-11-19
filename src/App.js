9e355880fa1d50246661c825c0b2baeaed808796
Luís Silva <luis.m.silva@mail.com>
2018-11-19 01:19:45 +0000

    Add known issues

--- src/App.js
+++ src/App.js
@@ -13,14 +13,22 @@ class App extends Component {
         <header className="app-header">
           <h1 className="app-title">Jogo do 24 / 24 Game</h1>
           <p>The 24 Game is an arithmetical card game in which the objective is to find a way to manipulate four integers so that the end result is 24.
-            For example, for the card with the numbers 4, 7, 8, 8, a possible solution is (7-(8/8))*4=24.<br></br>
+            For example, for the card with the numbers 6, 3, 7, 1, a possible solution is 6*3+7-1=24.<br></br>
             Read more @ <a href="https://en.wikipedia.org/wiki/24_Game" target="_blank" rel="noopener noreferrer">Wikipedia</a></p>
-          <small>
-            <b>Instructions:</b><br></br>
-            - Click on each number and signs in the correct order, filling the blanks bellow the card.<br></br>
-            - When all blanks are filled submit your try and see the result!<br></br>
-            - The square in the card works as reset, and it will clear the numbers and load a new card.
-          </small>
+          <p>
+            <small>
+              <b>Instructions:</b><br></br>
+              - Click on each number and signs in the correct order, filling the blanks bellow the card.<br></br>
+              - When all blanks are filled submit your try and see the result!<br></br>
+              - The square in the card works as reset, and it will clear the numbers and load a new card.
+            </small>
+          </p>
+          <p>
+            <small>
+              <b>Known Issues:</b><br></br>
+              - Calculator doesn't support yet parenthesis, so the solution (7-8/8)*4=24 it's still not possible to achieve.
+            </small>
+          </p>
         </header>
 
         <Board />
