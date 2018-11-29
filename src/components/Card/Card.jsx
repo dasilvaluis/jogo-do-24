6861f0f5627d0b2f6eff5ac86b385d9accc92842
Luís Silva <luis.m.silva@mail.com>
2018-11-29 01:29:51 +0000

    FIx ESlist errors

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Card extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
@@ -10,89 +10,137 @@ class Card extends Component {
         { disabled: false },
         { disabled: false },
         { disabled: false },
-        { disabled: false }
+        { disabled: false },
       ],
     };
   }
 
-  render() {
-    const { disabled, numberButtons } = this.state;
-    return (
-      <div className="card">
-        <div>
-          <span className="card__triangle"></span>
-          <span className="card__triangle"></span>
-          <span className="card__triangle"></span>
-          <span className="card__triangle"></span>
-        </div>
-        <button className="card__submit-button" onClick={() => this.handleTryCard()}></button>
-        <div>
-          <button className="card__number" disabled={numberButtons[0].disabled || disabled}
-              data-index="0"
-              data-value={this.props.numbers[0]}
-              onClick={(e) => this.handleNumberClick(e)}>
-            {this.props.numbers[0]}
-          </button>
-          <button className="card__number" disabled={numberButtons[1].disabled || disabled}
-              data-index="1"
-              data-value={this.props.numbers[1]}
-              onClick={(e) => this.handleNumberClick(e)}>
-            {this.props.numbers[1]}
-          </button>
-          <button className="card__number" disabled={numberButtons[2].disabled || disabled}
-              data-index="2"
-              data-value={this.props.numbers[2]}
-              onClick={(e) => this.handleNumberClick(e)}>
-            {this.props.numbers[2]}
-          </button>
-          <button className="card__number" disabled={numberButtons[3].disabled || disabled}
-              data-index="3"
-              data-value={this.props.numbers[3]}
-              onClick={(e) => this.handleNumberClick(e)}>
-            {this.props.numbers[3]}
-          </button>
-        </div>
-      </div>
-    )
-  }
-
+  /**
+   * Reset component state
+   *
+   * @returns {void}
+   */
   reset() {
     // Reset disabled state
-    let _numberButtons = this.state.numberButtons;
-    _numberButtons.map((x) => x.disabled = false)
+    let { numberButtons } = this.state;
+    numberButtons = [
+      { disabled: false },
+      { disabled: false },
+      { disabled: false },
+      { disabled: false },
+    ];
 
     this.setState({
-      numberButtons: _numberButtons
+      numberButtons,
     });
   }
 
-  handleTryCard() {
+  /**
+   * Handle try card action
+   *
+   * @returns {void}
+   */
+  handleResetCard() {
     this.reset();
 
     // Parent callback functions
-    this.props.onTryCard();
+    this.props.onResetCard();
   }
 
+  /**
+   * Handle click on number
+   *
+   * @param {event} e DOM Event
+   * @returns {void}
+   */
   handleNumberClick(e) {
+    const { numberButtons } = this.state;
     // Disabled clicked number
     const numberIndex = e.target.getAttribute('data-index');
-    let _numberButtons = this.state.numberButtons;
-    _numberButtons[numberIndex].disabled = true;
+    numberButtons[numberIndex].disabled = true;
 
     // Set State
     this.setState({
-      numberButtons: _numberButtons
+      numberButtons,
     });
 
     // Parent callback functions
     const numberValue = e.target.getAttribute('data-value');
     this.props.onNumberClicked(numberValue);
   }
+
+  render() {
+    const { disabled, numberButtons } = this.state;
+    const { numbers } = this.props;
+
+    return (
+      <div className="card">
+        <div>
+          <span className="card__triangle" />
+          <span className="card__triangle" />
+          <span className="card__triangle" />
+          <span className="card__triangle" />
+        </div>
+        <button type="button" className="card__submit-button" onClick={() => this.handleResetCard()} />
+        <div>
+          <button
+            type="button"
+            className="card__number"
+            disabled={numberButtons[0].disabled || disabled}
+            data-index="0"
+            data-value={numbers[0]}
+            onClick={e => this.handleNumberClick(e)}
+          >
+            {numbers[0]}
+          </button>
+          <button
+            type="button"
+            className="card__number"
+            disabled={numberButtons[1].disabled || disabled}
+            data-index="1"
+            data-value={numbers[1]}
+            onClick={e => this.handleNumberClick(e)}
+          >
+            {numbers[1]}
+          </button>
+          <button
+            type="button"
+            className="card__number"
+            disabled={numberButtons[2].disabled || disabled}
+            data-index="2"
+            data-value={numbers[2]}
+            onClick={e => this.handleNumberClick(e)}
+          >
+            {numbers[2]}
+          </button>
+          <button
+            type="button"
+            className="card__number"
+            disabled={numberButtons[3].disabled || disabled}
+            data-index="3"
+            data-value={numbers[3]}
+            onClick={e => this.handleNumberClick(e)}
+          >
+            {numbers[3]}
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 Card.defaultProps = {
-  numbers: [ 0, 0, 0, 0 ],
+  numbers: [0, 0, 0, 0],
   grade: 0,
+  onNumberClicked: () => {},
+  onResetCard: () => {},
+};
+
+Card.propTypes = {
+  numbers: PropTypes.instanceOf(Array),
+  grade: PropTypes.number,
+  onNumberClicked: PropTypes.func,
+  onResetCard: PropTypes.func,
 };
 
 export default Card;
