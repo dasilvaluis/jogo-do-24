457ebd04266ac037056b8592c7a62b2ed2219772
Luís Silva <luis.m.silva@mail.com>
2020-03-14 00:43:14 +0000

    Add printing link

--- src/pages/Game/Game.jsx
+++ src/pages/Game/Game.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Board from '../../components/Board';
 import Difficulty from '../../components/Difficulty/Difficulty';
 import './game.css';
-
+import '../../styles/css/helpers.css';
 
 export default () => (
   <div className="game-container">
@@ -11,5 +12,6 @@ export default () => (
       Difficulty: <Difficulty />
     </div>
     <Board />
+    <small className="h-display-block h-margintop-small">For the print version of all cards click <Link to="/print">here</Link></small>
   </div>
 );
