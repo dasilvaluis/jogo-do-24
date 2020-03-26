9817c13adf138093c7dfe1b367d5593e1f523b6e
Luís Silva <luis.m.silva@mail.com>
2020-03-26 14:09:15 +0000

    fix

--- src/server/index.js
+++ src/server/index.js
@@ -3,8 +3,8 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../../build')));
 
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../../build', 'index.html')));
 
 app.listen(process.env.PORT || 8080);
